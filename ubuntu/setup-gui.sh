6fa38623068fb40868ed77a8df8eb520db747f7a
Mickael <mickael.desmousseaux@rapid4cloud.com>
2026-02-08 12:33:50 +0700

    Clean the script

--- ubuntu/setup-gui.sh
+++ ubuntu/setup-gui.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
+# Update and upgrade packages
 sudo apt update && sudo apt upgrade -y
-sudo apt install xfce4 xfce4-goodies -y
-sudo apt install xrdp -y
 
+# Install GUI environment
+sudo apt install xfce4 xfce4-goodies -y
+
+# Install xrdp
+sudo apt install xrdp -y
 echo xfce4-session > ~/.xsession
 chmod 600 ~/.xsession
-
 sudo systemctl enable xrdp
 sudo systemctl restart xrdp
 
-# Ask about password change (default Yes)
+# Prompt for password change
+echo "========================================"
 read -p "Do you want to change the Ubuntu user password? (Y/n): " change_pwd < /dev/tty
 
 if [[ ! "$change_pwd" =~ ^[Nn]$ ]]; then
-    echo "Enter new password for user 'ubuntu':"
     sudo passwd ubuntu < /dev/tty
 else
     echo "Skipping password change."
 fi
+echo "========================================"
 
+# Show status of xrdp
 sudo systemctl status xrdp
 
 echo "================"
